943ead970fbc311f4a028e78b42bfc91aa853e92
Luis Villalobos <ivillalobosrivera@yahoo.com>
2022-12-24 08:45:07 -0600

    algorithms challenge 1 completed

--- section1ArraysAndBigONotation/src/com/sortalgorithms/Sort.java
+++ section1ArraysAndBigONotation/src/com/sortalgorithms/Sort.java
@@ -5,10 +5,12 @@ public class Sort {
     /**
      * From left to right
      * Compares and swap to the right if its greater
+     * in place algorithm
+     * O (n2)
+     * stable algortihm
      * @param array
-     * @return
      */
-    public static int[] bubble(int[] array){
+    public static void bubble(int[] array){
 
         for (int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--){
             for (int i = 0; i < lastUnsortedIndex; i++) {
@@ -18,17 +20,17 @@ public class Sort {
             }
         }
 
-        return array;
-
     }
 
     /**
      * From left to right
      * Compares who is the greatest to select its index to put to the right
+     * in place algorithm
+     * O (n2)
+     * Unstable algorithm
      * @param array
-     * @return
      */
-    public static int[] selection(int[] array){
+    public static void selection(int[] array){
 
         for (int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--){
 
@@ -39,14 +41,10 @@ public class Sort {
                 if(array[i] > array[largest]){
                     largest = i;
                 }
-
             }
 
             Sort.swap(array, largest, lastUnsortedIndex);
-
         }
-
-        return array;
 
     }
 
@@ -54,10 +52,12 @@ public class Sort {
     /**
      * From right to left
      * compares who is minus to open a space in its correct position
+     * in place algorithm
+     * O (n2)
+     * Stable algorithm
      * @param array
-     * @return
      */
-    public static int[] insertion(int[] array) {
+    public static void insertion(int[] array) {
 
         for(int firstUnsortedIndex = 1; firstUnsortedIndex < array.length; firstUnsortedIndex++){
 
@@ -74,7 +74,6 @@ public class Sort {
             array[i] = newElement;
         }
 
-        return array;
 
     }
 
@@ -82,10 +81,12 @@ public class Sort {
      * From right to Left
      * is a variation of insertionSort with the difference that it first sort by halfs
      * what are lower or higher
+     * In place algorithm
+     * O (n2) - In worst case but performs better than insertion sort in majority cases
+     * Unstable algorithm
      * @param array
-     * @return
      */
-    public static int[] shell(int[] array){
+    public static void shell(int[] array){
 
         for(int gap = array.length / 2; gap > 0; gap /= 2){
 
@@ -108,9 +109,18 @@ public class Sort {
 
         }
 
-        return array;
     }
 
+    /**
+     * Divide and conquer algortihm, it divides all the array in halfs recursively to get only an array
+     * from only one position, and when it happens returns sorting by halfs and saving in auxiliar arraya
+     * Not in place algorthm
+     * O (nlogn) base 2
+     * Stable algortihm
+     * @param array
+     * @param start
+     * @param end
+     */
     public static void merge(int[] array, int start, int end){
 
         if(end - start < 2){
@@ -141,6 +151,134 @@ public class Sort {
         System.arraycopy(array, i, array, start + tempIndex, mid - i);
         System.arraycopy(temp, 0, array, start, tempIndex);
 
+    }
+
+
+    /**
+     * It selects a pivot and compares from left to right and right to left, positioning to left
+     * pivot elements that are less than pivot and right pivot elements that are greater than pivot
+     * In-place algorithm
+     * O(nlogn) base 2, in the worst cacse its complexity will be O(n2)
+     * Unstable algorithm
+     * @param array
+     */
+    public static void quick(int[] array, int start, int end){
+
+        if(end - start < 2){
+            return;
+        }
+
+        int pivotIndex = partition(array, start, end);
+        quick(array, start, pivotIndex);
+        quick(array, pivotIndex + 1, end);
+
+    }
+
+    private static int partition(int[] array, int start, int end){
+
+        int pivot = array[start];
+        int i = start;
+        int j = end;
+
+        while (i < j){
+
+            while (i < j && array[--j] >= pivot);
+            if(i < j){
+                array[i] = array[j];
+            }
+
+            while (i < j && array[++i] <= pivot);
+            if(i < j){
+                array[j] = array[i];
+            }
+
+        }
+
+        array[j] = pivot;
+        return j;
+
+    }
+
+    /**
+     * It doesn't need comparisioons, assumes that values into array are minus or equal to
+     * the size of the array, for example: if ypu have an array of lenght ten, the array content
+     * should have ints form 0 to ten.
+     * It counts how manny numbers are in the array and put it count in its position
+     * then sort by the count
+     * Not in-place algotithm
+     * O (n)
+     * stable algorithm with extra steps
+     * @param array
+     */
+    public static void counting(int[] array, int min, int max){
+
+        int[] countingArray = new int[(max - min) + 1];
+
+        for (int i = 0; i < array.length; i++){
+
+            countingArray[array[i] - min]++;
+        }
+
+        int j = 0;
+
+        for (int i = min; i <= max; i++) {
+
+            while (countingArray[i - min] > 0){
+                array[j++] = i;
+                countingArray[i - min]--;
+            }
+        }
+    }
+
+
+    /**
+     * conditions to use: All the elements should have the same width and they have to be based on the combination for each postion (radix)
+     * Radix is the number of variants that an element of array can have for example: for every number in position you have a radix of 10
+     * because you only have (0-9) for each position
+     *
+     * Radix sort starts comparing every position in the content of every position in the array and compares
+     * with the same position to other position in the array, for example imagine that ypu have 1234 and 5678, radix sdtarts comparin unitys, and then
+     * tens, cents, etc. but you have to make it stable, you have tou use counting sort to take an order, for example if you have
+     * the same number position in 2 different contents (1234 5674 - 4 repeats) you have to count how many numers follow it and then return int by its count
+     * @param array
+     * @param radix
+     * @param width
+     */
+    public static void radix(int[] array, int radix, int width){
+        for (int i = 0; i < width; i++) {
+            radixSingleSort(array, i, radix);
+        }
+    }
+
+    private static void radixSingleSort(int[] array, int position, int radix) {
+
+        int numItems = array.length;
+
+        int[] countArray = new int[radix];
+
+        for (int value: array){
+            countArray[getDigit(position, value, radix)]++;
+        }
+
+        // Adjust the count array
+        for(int j = 1; j < radix; j++) {
+            countArray[j] += countArray[j - 1];
+        }
+
+        int[] temp = new int[numItems];
+
+        for(int tempIndex = numItems - 1; tempIndex >= 0; tempIndex--) {
+            temp[--countArray[getDigit(position, array[tempIndex], radix)]] = array[tempIndex];
+        }
+
+        for(int tempIndex = 0; tempIndex < numItems; tempIndex++){
+            array[tempIndex] = temp[tempIndex];
+        }
+
+    }
+
+    private static int getDigit(int position, int value, int radix) {
+        return value / (int) Math.pow(radix, position) % radix;
     }
 
     private static void swap(int[] array, int i, int j){
--- section1ArraysAndBigONotation/src/com/sortalgorithms/Test.java
+++ section1ArraysAndBigONotation/src/com/sortalgorithms/Test.java
@@ -4,22 +4,29 @@ public class Test {
 
     public static void main(String[] args) {
 
-        int[] arrayToTest = new int[]{ 20, 23, 24, 21};
+        //int[] arrayToTest = new int[]{ 20, 23, 24, 21};
         //int[] arrayToTest = new int[]{ 20, 35, -15, 7, 55, 1, -22 };
+        //int[] arrayToTest = new int[]{ 2, 5, 9, 8, 2, 8, 7, 10, 4, 3 };
+        int[] arrayToTest = new int[]{ 4725, 4586, 1330, 8792, 1594, 5729 };
         System.out.println("Unsorted numbers: ");
         Test.printArray(arrayToTest);
         //System.out.println("BubbleSort: ");
-        //Test.printArray(Sort.bubble(arrayToTest));
+        //Sort.bubble(arrayToTest);
         //System.out.println("SelectionSort: ");
-        //Test.printArray(Sort.selection(arrayToTest));
+        //Sort.selection(arrayToTest);
         //System.out.println("InsertionSort: ");
-        //Test.printArray(Sort.insertion(arrayToTest));
+        //Sort.insertion(arrayToTest);
         //System.out.println("ShellSort: ");
-        //Test.printArray(Sort.shell(arrayToTest));
-        System.out.println("MergeSort: ");
-        Sort.merge(arrayToTest,0, arrayToTest.length);
+        //Sort.shell(arrayToTest);
+        //System.out.println("MergeSort: ");
+        //Sort.merge(arrayToTest,0, arrayToTest.length);
+        //System.out.println("QuickSort: ");
+        //Sort.quick(arrayToTest, 0, arrayToTest.length);
+        //System.out.println("CountingSort: ");
+        //Sort.counting(arrayToTest, 1, 10);
+        System.out.println("RadixSort: ");
+        Sort.radix(arrayToTest, 10, 4);
         Test.printArray(arrayToTest);
-        //Test.printArray();
 
     }
 
